ccf368d3f36ac5f5c7d2f5fb2205f5759d4e7b51
Kipkemei De Isaac <pkemey@gmail.com>
2017-08-29 01:11:14 +0300

    decoupled

--- duka/settings.py
+++ duka/settings.py
@@ -1,23 +1,35 @@
 import os
-from django.core.urlresolvers import reverse_lazy
+from decouple import config
 from django.contrib import messages
-
+from django.core.urlresolvers import reverse_lazy
+ADMINS = config('ADMINS')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
+AUTH_USER_MODEL = 'authtools.User'
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-SECRET_KEY = '&4d%okc1wx6r#y&*&h2q63v=p+=92orqiy)l)%v$-s*8q8o4d='
-DEBUG = True
-
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'deisaack@gmail.com'
-EMAIL_HOST_PASSWORD = 'Jacktone1'
-EMAIL_PORT =587
-EMAIL_USE_TLS = True
-
-DEFAULT_FROM_EMAIL = "Isaac <deisaack@gmail.com>"
-
-
-ADMINS = [('Isaac', EMAIL_HOST_USER)]
-MANAGERS = ADMINS
-
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PWD_KEY'),
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+# import dj_database_url
+# db_from_env = dj_database_url.config()
+# DATABASES['default'].update(db_from_env)
+DEBUG = config('DEBUG', cast=bool)
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+# GEOPOSITION_GOOGLE_MAPS_API_KEY = 'AIzaSyD2XP6khyd123inKasJ2NxfWodaqYO_HqY'
+GEOPOSITION_GOOGLE_MAPS_API_KEY = config('GEOPOSITION_GOOGLE_MAPS_API_KEY')
+GOOGLE_RECAPTCHA_SECRET_KEY = config('GOOGLE_RECAPTCHA_SECRET_KEY')
 INSTALLED_APPS = [
     'rest_framework',
 
@@ -32,7 +44,6 @@ INSTALLED_APPS = [
     'duka.accounts',
     'duka.profiles',
     'duka.favorites',
-    # 'duka.favorites.api',
 
     'crispy_forms',
     'authtools',
@@ -40,7 +51,16 @@ INSTALLED_APPS = [
     'easy_thumbnails',
     'geoposition',
 ]
-
+LANGUAGE_CODE = 'en-us'
+LIVE_DIR = os.path.join(BASE_DIR, "live")
+LOGIN_REDIRECT_URL = reverse_lazy("home")
+LOGIN_URL = reverse_lazy("accounts:login")
+MANAGERS = ADMINS
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(LIVE_DIR, 'live/media')
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
@@ -51,9 +71,20 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAdminUser',
+    ],
+    'PAGE_SIZE': 6
+}
 ROOT_URLCONF = 'duka.urls'
-
+SECRET_KEY = config('SECRET_KEY')
+SITE_ID = 1
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(LIVE_DIR, "static")
+STATIC_URL = '/static/'
+THUMBNAIL_EXTENSION = 'png'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -69,69 +100,21 @@ TEMPLATES = [
         },
     },
 ]
-
+TIME_ZONE = 'Africa/Nairobi'
+USE_L10N = True
+USE_I18N = True
+USE_TZ = True
 WSGI_APPLICATION = 'duka.wsgi.application'
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'duka_db',
-#         'USER': 'duka_user',
-#         'PASSWORD': 'duka_pwd',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
-# }
-# import dj_database_url
-# db_from_env = dj_database_url.config()
-# DATABASES['default'].update(db_from_env)
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-SITE_ID = 1
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'Africa/Nairobi'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-# STATIC_ROOT = os.path.join(BASE_DIR, 'live/static')
-LIVE_DIR = os.path.join(BASE_DIR, "live")
-STATIC_ROOT = os.path.join(LIVE_DIR, "static")
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(LIVE_DIR, 'live/media')
-GEOPOSITION_GOOGLE_MAPS_API_KEY = 'AIzaSyCKKERHiiwDU37DQ719Uj93bXVlSGRMn9U'
-AUTH_USER_MODEL = 'authtools.User'
-LOGIN_REDIRECT_URL = reverse_lazy("home")
-LOGIN_URL = reverse_lazy("accounts:login")
-
-THUMBNAIL_EXTENSION = 'png'
-RISPY_TEMPLATE_PACK = 'bootstrap3'
-
-MESSAGE_TAGS = {
-    messages.ERROR: 'danger'
-}
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0', 'duka.herokuapp.com']
-
-# if DEBUG:
-#     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db3.sqlite3'),
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db3.sqlite3'),
+        }
     }
-}
-
 if 'TRAVIS' in os.environ:
     DATABASES = {
         'default': {
@@ -143,12 +126,3 @@ if 'TRAVIS' in os.environ:
             'PORT':     '',
         }
     }
-
-CRISPY_TEMPLATE_PACK = 'bootstrap3'
-
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAdminUser',
-    ],
-    'PAGE_SIZE': 6
-}
